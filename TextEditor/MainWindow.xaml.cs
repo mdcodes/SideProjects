fdbc7363c0f04f3ac9328c83444675c9050694c9
Michal Drahorat <mdrahorat4563@conestogac.on.ca>
2017-09-03 17:03:59 -0400

    First Iteration of the Text Editor!

---
+++ TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace BudgetMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string[] contentsToWrite = txtCompose.Text.Split('S');
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File|*.txt";
+            sfd.Title = "Save your text file";
+            sfd.ShowDialog();
+
+            File.WriteAllLines(sfd.FileName, contentsToWrite);
+            
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            string[] contentsToLoad = File.ReadAllLines(ofd.FileName);
+
+            foreach (string word in contentsToLoad)
+            {
+                txtCompose.Text += word;
+            }
+        }
+    }
+}
